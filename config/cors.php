77f70fc792d15077b1b3cae47d78853cf68145cd
Olugbenga Raymond <tutorialcenter001@gmail.com>
2026-05-07 18:47:39 +0100

    Adding affiliate to cors

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'https://tutorialcenter-rho.vercel.app', 'https://tutorialcenter.africa'],
+    'allowed_origins' => ['http://localhost:3000', 'https://tutorialcenter-rho.vercel.app', 'https://tutorialcenter.africa', 'https://affiliates.tutorialcenter.africa'],
 
     'allowed_origins_patterns' => [],
 
